48c6b7876fedf5b17aa2f9688396e021019d7c2b
loxtank <lars.daniel.andersson@gmail.com>
2016-01-02 15:56:29 +0100

    Added singleProject variable

--- projects/router/routes.js
+++ projects/router/routes.js
@@ -10,15 +10,17 @@ FlowRouter.route("/projects/add", {
   }
 });
 
-// Return a single JSON object containing the specified project.
+// Add route to return a single JSON object containing the specified project.
 JsonRoutes.add("get", "/projects/:projectId", function (req, res, next) {
-  var projectId = req.params.projectId;
+  var projectId = req.params.projectId; // The project id, in MongoDB
+  var singleProject = Projects.findOne(projectId) // Sends the result to client
 
-  JsonRoutes.sendResult(res, 200, Projects.findOne(projectId));
+  JsonRoutes.sendResult(res, 200, singleProject);
 });
 
 // Returns JSON array containing all projects in Projects collection.
 JsonRoutes.add("get", "/projects", function (req, res, next) {
-    var allProjects = Projects.find().fetch();
-  JsonRoutes.sendResult(res, 200, allProjects);
+  var allProjects = Projects.find().fetch(); // Finds all projects and returns array
+
+  JsonRoutes.sendResult(res, 200, allProjects); // Sends the result to client
 });
