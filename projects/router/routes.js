ae7e1bda3985b00e433e26f3ace8ab9005edb954
loxtank <lars.daniel.andersson@gmail.com>
2016-01-03 13:49:03 +0100

    Changed JSON routes

--- projects/router/routes.js
+++ projects/router/routes.js
@@ -11,7 +11,7 @@ FlowRouter.route("/projects/add", {
 });
 
 // Add route to return a single JSON object containing the specified project.
-JsonRoutes.add("get", "/projects/:projectId", function (req, res, next) {
+JsonRoutes.add("get", "/projects/:projectId/json", function (req, res, next) {
   var projectId = req.params.projectId; // The project id, in MongoDB
   var singleProject = Projects.findOne(projectId) // Finds matching project and returns object.
 
@@ -19,7 +19,7 @@ JsonRoutes.add("get", "/projects/:projectId", function (req, res, next) {
 });
 
 // Returns JSON array containing all projects in Projects collection.
-JsonRoutes.add("get", "/projects", function (req, res, next) {
+JsonRoutes.add("get", "/projects/json", function (req, res, next) {
   var allProjects = Projects.find().fetch(); // Finds all projects and returns array
 
   JsonRoutes.sendResult(res, 200, allProjects); // Sends the result to client
