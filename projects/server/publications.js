f4a119c3f40b6dfc2ef79cb9942b409ac7b10608
leosa83 <leosa@kth.se>
2016-01-18 11:26:55 +0000

    removed unneccesary code and commented

--- projects/server/publications.js
+++ projects/server/publications.js
@@ -1,7 +1,4 @@
-if (Meteor.isServer) {
-  // This code only runs on the server
-
-    Meteor.publish("singleProject", function (projectId) {
-          return Projects.find({ _id: projectId });
-    });
-}
+//The single singleProject publication return a cursors with a single project using the projectId as an argument
+Meteor.publish("singleProject", function (projectId) {
+      return Projects.find({ _id: projectId });
+});
