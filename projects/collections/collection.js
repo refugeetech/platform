d25c67e7d6bef3659d7a1b84735bde720bfbd3c2
loxtank <lars.daniel.andersson@gmail.com>
2016-01-21 23:17:48 +0100

    problemCategofy dropdown completed, location started

--- projects/collections/collection.js
+++ projects/collections/collection.js
@@ -3,10 +3,30 @@ Projects = new Mongo.Collection('projects');
 //Add search index with easy search
 ProjectsIndex  = new EasySearch.Index({
     collection: Projects,
-    fields: ['name','tags','postalAdress'],
-    engine: new EasySearch.Minimongo()
+    fields: ['name','tags','postalAddress','problemCategory'],
+    engine: new EasySearch.MongoDB({
+      selector: function (searchObject, options, aggregation) {
+        let selector = this.defaultConfiguration().selector(searchObject, options, aggregation),
+          categoryFilter = options.search.props.categoryFilter;
+          // locationFilter = options.search.props.locationFilter;
+
+          console.log(categoryFilter);
+          // console.log(locationFilter);
+
+        if (_.isString(categoryFilter) && !_.isEmpty(categoryFilter)) {
+          selector.problemCategories = categoryFilter;
+        }
+
+        //if (_.isString(locationFilter) && !_.isEmpty(locationFilter)) {
+        //  selector.postalAddress.$.country= locationFilter;
+        //}
+
+        return selector;
+      }
+  })
+
   });
-  
+
 ProjectMedia = new FS.Collection("projectMedia", {
   stores: [new FS.Store.GridFS("projectMediaStore")]
 });
--- projects/view/client/allProjects/viewProjects.js
+++ projects/view/client/allProjects/viewProjects.js
@@ -1,8 +1,11 @@
-Template.viewProjects.helpers({
-  "projects": function () {
-    // Get all projects
-    var projects = Projects.find();
-
-    return projects;
+// Filters searchResult when category-filter dropdown value is changed
+Template.viewProjects.events({
+  'change .category-filter': function (e) {
+    ProjectsIndex.getComponentMethods()
+      .addProps('categoryFilter', $(e.target).val());
   }
+  // 'change .location-filter': function (e) {
+  //   ProjectsIndex.getComponentMethods()
+  //     .addProps('locationFilter', $(e.target).val());
+  // }
 });
