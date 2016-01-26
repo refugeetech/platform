7f846af95aaab2b4f496957af9edd18728afd64e
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-26 14:26:17 +0200

    Selectize when template subscriptions ready

--- projects/add/client/addProject.js
+++ projects/add/client/addProject.js
@@ -1,9 +1,53 @@
-Template.addProject.onCreated = function () {
+Template.addProject.created = function () {
   // Get reference to template instance
-  const instance = this;
+  let instance = this;
 
   // Subscribe to all tags, for tag auto-complete field
   instance.subscribe("allTags");
+};
+
+Template.addProject.rendered = function() {
+  // Get reference to template instance
+  let instance = this;
+
+  instance.autorun(function () {
+    if (instance.subscriptionsReady()) {
+      // Get all existing tags
+      let tagOptions = Tags.find().fetch();
+
+      $('#tags').selectize({
+          delimiter: ',',
+          persist: false,
+          valueField: '_id',
+          labelField: 'name',
+          searchField: 'name',
+          create: true,
+          highlight: true,
+          maxOptions: 5,
+          options: tagOptions,
+          onItemAdd: function (tag) {
+            // Check to see if tag exists in Tags collection
+            var existingTag = Tags.findOne({"name": tag});
+
+            // placeholder for new tag ID
+            let tagId;
+
+            // If tag doesn't exist
+            if (!existingTag ) {
+              // Add the tag to the Tags collection
+              tagId = Tags.insert({"name": tag});
+
+              // TODO: figure out how to limit duplicate tags
+              // e.g. 'Beans' and 'beans'
+              // unless this is not an issue
+            };
+
+            // Return a tag object containing tag ID
+            return {value: tagId, text: tag};
+          }
+      });
+    }
+  });
 };
 
 Template.addProject.helpers({
