d928a680981a3064b7d2f909663085d66d7e2f34
Angamanga <ki222bj@student.lnu.se>
2016-01-26 17:20:10 +0100

    changing input names

--- projects/add/client/addProject.js
+++ projects/add/client/addProject.js
@@ -14,13 +14,6 @@ Template.addProject.rendered = function() {
     if (instance.subscriptionsReady()) {
       // Get all existing tags
       let tagOptions = Tags.find().fetch();
-
-      // This function definition may not be necessary, so commenting out
-      // var tagCallback = function (tagObject) {
-      //   console.log("inside callback", tagObject);
-      //   return tagObject;
-      // };
-
       $('#tags').selectize({
           delimiter: ',',
           persist: false,
@@ -31,27 +24,23 @@ Template.addProject.rendered = function() {
           highlight: true,
           maxOptions: 5,
           options: tagOptions,
-          create: function (tagValue, tagCallback) {
+          create: function (input,callback) {
+    
             // Add the tag to the Tags collection
-            let tagId = Tags.insert({"name": tagValue});
-
-            // Construct a tag object with text/value
-            let tagObject = {
-              value: tagId,
-              text: tagValue
-            };
-
-            // The tags object works here
-            console.log(tagObject);
-
-            // However, I am uncertain how to get the tag callback to work
-            // so that the newly created tag will be added to the Selectize widget
-            tagCallback(tagObject);
-          }
+           Tags.insert({"name": input}, function(error,id){
+              if(!error){
+                console.log('inside no error');
+                callback({value: id,text: input}); 
+              }
+                          });
+          },
+          selectOnTab:true
+        
       });
     }
-  });
-};
+
+});
+}
 
 Template.addProject.helpers({
   existingTags: function () {
