ca176ccea416f8f1da43d65085042a73dbdbd70d
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-26 13:04:31 +0200

    Add existingTags helper

--- projects/add/client/addProject.js
+++ projects/add/client/addProject.js
@@ -5,3 +5,12 @@ Template.addProject.onCreated = function () {
   // Subscribe to all tags, for tag auto-complete field
   instance.subscribe("allTags");
 };
+
+Template.addProject.helpers({
+  existingTags: function () {
+    // Get all existing tags
+    let tags = Tags.find().fetch();
+
+    return tags;
+  }
+});
