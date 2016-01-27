e0aa3cb15d0ab7d49dd56d7cab9f9570393052f8
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-27 13:28:04 +0200

    Cleanup code

--- projects/server/publications.js
+++ projects/server/publications.js
@@ -1,4 +1,5 @@
-//The single singleProject publication return a cursors with a single project using the projectId as an argument
 Meteor.publish("singleProject", function (projectId) {
-      return Projects.find({ _id: projectId });
+  // Return a cursor with a single project
+  // using the projectId as an argument
+  return Projects.find({ _id: projectId });
 });
