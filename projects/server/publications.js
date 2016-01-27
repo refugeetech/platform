60fa4571be5641f0fba9a6a3ea8bceb9d75b58e4
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-01-27 13:28:40 +0200

    Add allProjects publication

--- projects/server/publications.js
+++ projects/server/publications.js
@@ -1,3 +1,9 @@
+Meteor.publish("allProjects", function () {
+  // Return a cursor with all projects
+  return Projects.find();
+});
+
+
 Meteor.publish("singleProject", function (projectId) {
   // Return a cursor with a single project
   // using the projectId as an argument
