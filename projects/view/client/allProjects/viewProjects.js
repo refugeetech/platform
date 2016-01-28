d99c972cd51a959a7d9e981dc63458a04e051445
loxtank <lars.daniel.andersson@gmail.com>
2016-01-28 13:27:08 +0100

    Cleanup

--- projects/view/client/allProjects/viewProjects.js
+++ projects/view/client/allProjects/viewProjects.js
@@ -1,20 +1,18 @@
-// Returns all countries for location dropdown
+// Returns countries for location dropdown
 Template.locationDropdown.helpers({
   countries : function() {
-    //returns an array of project objects
-    var out =  Projects.find();
-    console.log(out);
-    console.log(out.fetch());
-    // lets just use a list initally
     var countries = [];
     var projects = Projects.find().fetch();
+    // For every project in collection...
     projects.forEach(function(project) {
       var country = project.postalAddress.country;
+      // Check if projects array contain country, if not...
       if(countries.indexOf(country)<0) {
+        // Add it to the array
         countries.push(country);
       }
     });
-
+    // Returns a String Array with all unique countries in Projects collection
     return countries;
   }
 
