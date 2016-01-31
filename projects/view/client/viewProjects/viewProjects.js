4126eee30e645863f39d0d2aa06ebe4b9f8d5645
Leo Salmonsson <leosa@kth.se>
2016-01-31 14:47:31 +0100

    Reverted the viewProjects js file which made the easysearch work again

---
+++ projects/view/client/viewProjects/viewProjects.js
@@ -0,0 +1,16 @@
+Template.viewProjects.created = function () {
+  // Get reference to template instance
+  let instance = this;
+
+  // Subscribe to all projects
+  instance.subscribe("allProjects");
+};
+
+Template.viewProjects.helpers({
+  "projects": function () {
+    // Get all projects
+    var projects = Projects.find().fetch();
+
+    return projects;
+    }
+ });
