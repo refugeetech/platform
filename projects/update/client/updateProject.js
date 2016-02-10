74764be981ac24b8a6c5c53a219aca1528c5c871
leosa83 <leosa@kth.se>
2016-02-10 10:04:15 +0000

    removed everything not used by the quickForm path taken by the updateProject template; removed the autoForm.js file and the .onRendered callback template function

--- projects/update/client/updateProject.js
+++ projects/update/client/updateProject.js
@@ -1,34 +1,3 @@
-Template.updateProject.rendered = function() {
-  // Get reference to template instance
-  let instance = this;
-
-  instance.autorun(function () {
-    // Make sure tags subscription is ready
-    if (instance.subscriptionsReady()) {
-      // Get all existing tags
-      let tagOptions = Tags.find().fetch();
-
-      $('#tags').selectize({
-        delimiter: ',',
-        persist: false,
-        valueField: 'name',
-        labelField: 'name',
-        searchField: 'name',
-        highlight: true,
-        maxOptions: 5,
-        options: tagOptions,
-        create:true,
-        onItemAdd(value){
-          // Insert tag into Tags collection if it doesn't exist
-          if(!Tags.findOne({"name":value})){
-            Tags.insert({"name":value});
-          }
-        }
-      });
-    }
-  });
-};
-
 Template.updateProject.helpers({
     //Using the Flowrouter package to get the route param projects/:projectId ; is set up in the Template.updateProject.onCreated callback
     project: function () {
@@ -39,9 +8,6 @@ Template.updateProject.helpers({
 Template.updateProject.onCreated(function(){
   // Get reference to template instance
   const instance = this;
-
-  // Subscribe to all tags, for tag auto-complete field
-  instance.subscribe("allTags");
   
   instance.autorun(function() {
     //subscribe to singleProject with the projectId from the route parameter
