9efc13d352fafc4f2d2b733da3f600dd01d98b30
Brylie Christopher Oxley <brylie@gnumedia.org>
2016-02-12 11:18:13 +0200

    Normalize whitespace

--- projects/view/client/projectProfile/projectProfile.js
+++ projects/view/client/projectProfile/projectProfile.js
@@ -1,8 +1,8 @@
 Template.projectProfile.helpers({
   //Using the Flowrouter package to get the route param projects/:projectId ; is set up in the Template.projectProfile.onCreated callback
-    project: function () {
-      return Projects.findOne({ _id: FlowRouter.getParam('projectId')});
-    },
+  project: function () {
+    return Projects.findOne({ _id: FlowRouter.getParam('projectId')});
+  },
 });
 
 Template.projectProfile.onCreated(function(){
