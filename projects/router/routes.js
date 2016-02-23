3633ccacca203d61bba070f0099d08af1838faf7
Angamanga <ki222bj@student.lnu.se>
2016-02-23 07:52:20 +0100

    commenting out routes

--- projects/router/routes.js
+++ projects/router/routes.js
@@ -1,23 +1,23 @@
-FlowRouter.route("/projects", {
-  action: function (params, queryParams) {
-    BlazeLayout.render("mainLayout", {main: "viewProjects"});
-  }
-});
-
-FlowRouter.route("/projects/add", {
-  action: function (params) {
-    BlazeLayout.render("mainLayout", {main: "addProject"});
-  }
-});
-
-FlowRouter.route('/projects/:projectId', {
-  action: function (params, queryParams) {
-    BlazeLayout.render('mainLayout', { main: 'projectProfile' });
-  }
-});
-
-FlowRouter.route('/projects/:projectId/update', {
-  action: function (params, queryParams) {
-    BlazeLayout.render('mainLayout', { main: 'updateProject' });
-  }
-});
+//FlowRouter.route("/projects", {
+//  action: function (params, queryParams) {
+//    BlazeLayout.render("mainLayout", {main: "viewProjects"});
+//  }
+//});
+//
+//FlowRouter.route("/projects/add", {
+//  action: function (params) {
+//    BlazeLayout.render("mainLayout", {main: "addProject"});
+//  }
+//});
+//
+//FlowRouter.route('/projects/:projectId', {
+//  action: function (params, queryParams) {
+//    BlazeLayout.render('mainLayout', { main: 'projectProfile' });
+//  }
+//});
+//
+//FlowRouter.route('/projects/:projectId/update', {
+//  action: function (params, queryParams) {
+//    BlazeLayout.render('mainLayout', { main: 'updateProject' });
+//  }
+//});
