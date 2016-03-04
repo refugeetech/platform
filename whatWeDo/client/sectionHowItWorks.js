0de7da9eed3193bb0e293d26c56462706c6ede30
Leo Salmonsson <leosa@kth.se>
2016-03-04 20:23:51 +0100

    buttons work, css done, not mobile friendly, use the carousel implementation for mobile

--- whatWeDo/client/sectionHowItWorks.js
+++ whatWeDo/client/sectionHowItWorks.js
@@ -1,26 +1,17 @@
 Template.sectionHowItWorks.events({
   'click .tab a'(event) {
-      event.preventDefault();
-      toggleContent($(event.target).attr('data-target'));
-      function toggleContent(target) {
-        $('.tab-content').each(function(i,e){
-          $(e).hide()
-        });
-        $('.tab .btn').each(function(i,e){
-          $(e).removeClass('active');
-        });
-        $("#"+target).show();
-      
-      }
-      $(event.target).addClass('active');
+    event.preventDefault();
+    toggleContent($(event.target).attr('data-target'));
+    function toggleContent(target) {
+      $('.tab-content').each(function(i,e){
+        $(e).hide()
+      });
+      $('.tab .btn').each(function(i,e){
+        $(e).removeClass('active');
+      });
+      $("#"+target).show();
+    
     }
-});
-
-Template.sectionHowItWorks.onRendered(function() {
-  const options = {
-    center:true,
-    items: 1,
-    nav:true
-  };
-  $(".owl-carousel").owlCarousel(options);
+    $(event.target).addClass('active');
+  }
 });
