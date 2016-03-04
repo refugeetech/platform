7df5cad1d5338e642916bc3bb7057fef996c1872
Angamanga <ki222bj@student.lnu.se>
2016-03-04 01:02:49 +0100

    whatwedostyles@

---
+++ whatWeDo/sectionHowItWorks.js
@@ -0,0 +1,9 @@
+if(Meteor.isClient) {
+    Template.sectionHowItWorks.events({
+        'click .button'(event) {
+            //event.preventDefault();
+            //console.log(event);
+            //$('.button').toggleClass('is-active');
+         }
+     });
+}
