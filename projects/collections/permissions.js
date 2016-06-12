78402cbb85c7f44e5817e310816d6bab6fad654e
leosa83 <leosa@kth.se>
2016-06-12 12:07:55 +0000

    added new package for handling api requests for api version 01 and configured some endpoints

--- projects/collections/permissions.js
+++ projects/collections/permissions.js
@@ -1,5 +1,5 @@
-Projects.allow({
-  insert: function () {
-    return true;
-  }
-});
+// Projects.allow({
+//   insert: function () {
+//     return true;
+//   }
+// });
---
+++ ratings/server/api/v01/ratingsApi.js
@@ -0,0 +1,20 @@
+  RatingsApiV01 = new Restivus({
+    useDefaultAuth: true,
+    prettyJson: true,
+    version:'v01',
+    defaultHeaders: {'Content-Type': 'application/json'}
+  });  
+  
+  // Generates: GET, POST on /api/v01/ratings and GET, PUT, DELETE on
+  // /api/v01/ratings/:id for the Ratings collection
+  Ratings = Mongo.Collection.get('ratings');
+  if(!Ratings) {
+    Ratings = new Mongo.Collection('ratings');
+  }
+  console.log("here");
+  console.log(Ratings);
+  RatingsApiV01.addCollection(Ratings);
+  
+ 
+
+
