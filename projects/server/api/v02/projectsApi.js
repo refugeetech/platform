03375acd4647620418b32b737b4156eb5e148744
Ahmed Yassine Boutahar <ayboutahar@gmail.com>
2016-10-02 00:16:20 +0100

    return {status:"exists"} when an email exists already

--- projects/server/api/v02/projectsApi.js
+++ projects/server/api/v02/projectsApi.js
@@ -1,45 +1,48 @@
-  var ProjectsApiV02 = new Restivus({
+var ProjectsApiV02 = new Restivus({
     useDefaultAuth: true,
     prettyJson: true,
-    version:'v02',
-    defaultHeaders: {'Content-Type': 'application/json; charset=UTF-8'}
-  });
+    version: 'v02',
+    defaultHeaders: {
+        'Content-Type': 'application/json; charset=UTF-8'
+    }
+});
 
-  // Generates: GET on /api/v01/projects
-  // /api/v01/projects/:id for the Projects collection
-  Projects = Mongo.Collection.get('projects');
-  if(!Projects) {
+// Generates: GET on /api/v01/projects
+// /api/v01/projects/:id for the Projects collection
+Projects = Mongo.Collection.get('projects');
+if (!Projects) {
     Projects = new Mongo.Collection('projects');
-  }
+}
 
-  ProjectsApiV02.addRoute('projects', {
-    get: function () {
-      return {
-        status: "success",
-        data: Projects.find().fetch()
-      };
+ProjectsApiV02.addRoute('projects', {
+    get: function() {
+        return {status: "success", data: Projects.find().fetch()};
     }
-  });
+});
 
-   ProjectsApiV02.addRoute('projects/:id', {
-    get: function () {
-      return {
-        status: "success",
-        data: Projects.findOne({_id:this.urlParams.id})
-      };
+ProjectsApiV02.addRoute('projects/:id', {
+    get: function() {
+        return {
+            status: "success",
+            data: Projects.findOne({_id: this.urlParams.id})
+        };
     }
-  });
+});
 
-  ProjectsApiV02.addRoute('newsletter/:email', {
-   get: function () {
-     var newsletteremail = {
-        "email": this.urlParams.email,
-        "dateAdded": new Date()
-       };
-     NewsletterEmailsSchema.validate(newsletteremail);
-     NewsletterEmails.insert(newsletteremail);
-     return {
-       status: "success"
-     };
-   }
- });
+ProjectsApiV02.addRoute('newsletter/:email', {
+    get: function() {
+        //check if already exists
+        if (NewsletterEmails.findOne({"email": this.urlParams.email})) {
+            return {status: "exists"};
+        } else {
+            var newsletteremail = {
+                "email": this.urlParams.email,
+                "dateAdded": new Date()
+            };
+            NewsletterEmailsSchema.validate(newsletteremail);
+            NewsletterEmails.insert(newsletteremail);
+            return {status: "success"};
+        }
+
+    }
+});
