446e6c4369e292e507c4b9f01edd64fc0c0fc582
Ahmed Yassine Boutahar <ayboutahar@gmail.com>
2016-11-27 14:36:38 +0000

    Temporary solution to the carousel bug

--- landing/client/sectionWhatTheySay.js
+++ landing/client/sectionWhatTheySay.js
@@ -1,18 +1,25 @@
+Template.sectionWhatTheySay.onCreated(function() {
+    var request = $.getJSON('data/mediaAppearances.json', function(data) {
+        Session.set('media', data.media);
+    });
+});
+
 Template.sectionWhatTheySay.helpers({
     media: function() {
-        var request = $.getJSON('data/mediaAppearances.json', function(data) {
-            Session.set('media', data.media);
-        });
         return Session.get('media');
     }
 });
 
 Template.sectionWhatTheySay.onRendered(function() {
-    const options = {
-        center: true,
-        items: 1,
-        margin: 0,
-        loop: true
-    };
-    $(".owl-carousel").owlCarousel(options);
+    //this is just a hack to the problem, we should look for a proper solution.
+    //problem: the carousel doesn't render when page refreshes
+    Meteor.setTimeout(function() {
+      const options = {
+          center: true,
+          items: 1,
+          margin: 0,
+          loop: true
+      };
+      $(".owl-carousel").owlCarousel(options);
+    }, 1000);
 });
